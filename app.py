b1e7b475d66da9af05a0b0b10eacec421e796230
sodiel <detopuse@gmail.com>
2024-10-14 20:55:47 +0300

    Добавляет доску

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 class TicTacToe:
     def __init__(self):
+        self.board = [' ']*9
         self.winning_combinations = [
             (0, 1, 2), (3, 4, 5), (6, 7, 8), # Строки
             (0, 3, 6), (1, 4, 7), (2, 5, 8), # Столбцы
